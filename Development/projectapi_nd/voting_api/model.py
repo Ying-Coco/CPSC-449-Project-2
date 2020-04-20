503ec8f55de333e73c5de982574b0060aab4706b
Ying <ywen1306@csu.fullerton.edu>
2020-04-20 15:10:45 -0700

    voting works

--- Development/projectapi_nd/voting_api/model.py
+++ Development/projectapi_nd/voting_api/model.py
@@ -11,17 +11,17 @@ class VoteModel:
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
         if not pID:
             return {'message': f'Please provide a post id.'}
-        #id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
-        #if id_check != None:
-        try:
-            query = f'UPDATE Post SET up_vote = (up_vote + 1) WHERE pID = "{pID}";'
-            self.conn.execute(query)
-            return {'message': 'Upvote success.'}
-            self.conn.commit()
-        except:
-            return {'message':'Could not upvote for {pID} you entered!'}
-        #else:
-        #    return {'message': f'Post not found!'} 
+        id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
+        if id_check:
+            try:
+                query = f'UPDATE Post SET up_vote = (up_vote + 1) WHERE pID = "{pID}";'
+                self.conn.execute(query)
+                return {'message': 'Upvote success.'}
+                self.conn.commit()
+            except:
+                return {'message':'Could not upvote for {pID} you entered!'}
+        else:
+            return {'message': f'Post not found!'} 
         self.conn.close()
 
 
@@ -30,31 +30,38 @@ class VoteModel:
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
         if not pID:
             return {'message': f'Please provide a post id.'}
-        #id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
-        #if id_check != None:
-        try:
-            query = f'UPDATE Post SET down_vote = (down_vote + 1) WHERE pID = "{pID,}";'
-            self.conn.execute(query)
-            return {'message': 'Downvote for {pID} is updated!'}
-            self.conn.commit()
-        except:
-            return {'message':'Could not downvote for {pID} you entered!'}
-        #else:
-            #return {'message': f'Post not found!'}             
+        id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
+        if id_check:
+            try:
+                query = f'UPDATE Post SET down_vote = (down_vote + 1) WHERE pID = "{pID,}";'
+                self.conn.execute(query)
+                return {'message': 'Downvote for {pID} is updated!'}
+                self.conn.commit()
+            except:
+                return {'message':'Could not downvote for {pID} you entered!'}
+        else:
+            return {'message': f'Post not found!'}             
         self.conn.close()
 
     #retrieve votes example: curl -i 'http://localhost:2015/votes/retrieve_votes?vote_id=2';
     def retrieve_votes(self, pID):
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
-        query = f'SELECT title, up_vote, down_vote FROM Post WHERE pID = "{pID}";'
-        result = self.conn.execute(query).fetchall()
-        return jsonify(list(result))
+        if not pID:
+            return {'message': f'Please provide a post id.'}
+        id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
+        if id_check:
+            try: 
+                query = f'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post WHERE pID = "{pID}";'
+                result = self.conn.execute(query).fetchall()
+                return jsonify(list(result))
+            except:
+                return {'message': 'Post not found!'}
         self.conn.close()
     
     #list top post by votes example: curl -i 'http://localhost:2015/votes/list_top_posts_byvotes?n=5'
     def list_top_posts_byvotes(self, n):
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
-        query = 'SELECT title, up_vote, down_vote FROM Post ORDER BY abs(up_vote - down_vote) DESC;'
+        query = 'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post ORDER BY abs(up_vote - down_vote) DESC;'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
         self.conn.close()
@@ -62,7 +69,7 @@ class VoteModel:
     #get list example: curl -i 'http://localhost:2015/votes/get_list?title=%te%'
     def get_list(self, title):
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
-        query = f'SELECT title, up_vote, down_vote FROM Post WHERE title LIKE "{title}" ORDER BY (up_vote - down_vote) DESC;'
+        query = f'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post WHERE title LIKE "{title}" ORDER BY (up_vote - down_vote) DESC;'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
         self.conn.close()
