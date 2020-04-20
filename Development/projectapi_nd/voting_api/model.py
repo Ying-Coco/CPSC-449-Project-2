d84f67bc093bca67d00e3f2808ffa48adc75145b
student <bobberino98@gmail.com>
2020-04-20 16:44:33 -0700

    votes fixed

--- Development/projectapi_nd/voting_api/model.py
+++ Development/projectapi_nd/voting_api/model.py
@@ -19,7 +19,7 @@ class VoteModel:
                 return {'message': 'Upvote success.'}
                 self.conn.commit()
             except:
-                return {'message':'Could not upvote for {pID} you entered!'}
+                return {'message':f'Could not upvote for {pID} you entered!'}
         else:
             return {'message': f'Post not found!'} 
         self.conn.close()
@@ -35,10 +35,10 @@ class VoteModel:
             try:
                 query = f'UPDATE Post SET down_vote = (down_vote + 1) WHERE pID = "{pID,}";'
                 self.conn.execute(query)
-                return {'message': 'Downvote for {pID} is updated!'}
+                return {'message': f'Downvote for {pID} is updated!'}
                 self.conn.commit()
             except:
-                return {'message':'Could not downvote for {pID} you entered!'}
+                return {'message':f'Could not downvote for {pID} you entered!'}
         else:
             return {'message': f'Post not found!'}             
         self.conn.close()
@@ -51,7 +51,7 @@ class VoteModel:
         id_check = self.conn.execute('SELECT * FROM Post WHERE pID = ?', (pID,)).fetchall()
         if id_check:
             try: 
-                query = f'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post WHERE pID = "{pID}";'
+                query = f'SELECT abs(up_vote - down_vote) AS total_vote FROM Post WHERE pID = "{pID}";'
                 result = self.conn.execute(query).fetchall()
                 return jsonify(list(result))
             except:
@@ -61,7 +61,7 @@ class VoteModel:
     #list top post by votes example: curl -i 'http://localhost:2015/votes/list_top_posts_byvotes?n=5'
     def list_top_posts_byvotes(self, n):
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
-        query = 'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post ORDER BY abs(up_vote - down_vote) DESC;'
+        query = 'SELECT title, abs(up_vote - down_vote) AS total_vote FROM Post ORDER BY abs(up_vote - down_vote) DESC;'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
         self.conn.close()
@@ -69,7 +69,7 @@ class VoteModel:
     #get list example: curl -i 'http://localhost:2015/votes/get_list?title=%te%'
     def get_list(self, title):
         self.conn = sqlite3.connect('../service.db', isolation_level=None)
-        query = f'SELECT title, up_vote, down_vote, abs(up_vote - down_vote) AS total_vote FROM Post WHERE title LIKE "{title}" ORDER BY (up_vote - down_vote) DESC;'
+        query = f'SELECT title, abs(up_vote - down_vote) AS total_vote FROM Post WHERE title LIKE "{title}" ORDER BY (up_vote - down_vote) DESC;'
         result = self.conn.execute(query).fetchall()
         return jsonify(list(result))
         self.conn.close()
---
+++ TestSuite/basic_test.sh
@@ -0,0 +1,147 @@
+#test script for user
+curl --verbose \
+     -X PUT \
+     --header 'Content-Type: application/json' \
+     --data @./Data/post.json \
+     http://localhost:2015/posts/create-post
+
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/title.json \
+     http://localhost:2015/posts/retrieve
+
+
+curl --verbose \
+     -X DELETE \
+     --header 'Content-Type: application/json' \
+     --data @./Data/title.json \
+     http://localhost:2015/posts/delete 
+
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/list_particular.json \
+     http://localhost:2015/posts/list-n-particular
+
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/list_recent.json \
+     http://localhost:2015/posts/list-recent
+
+curl --verbose \
+    -X PUT \
+    --header 'Content-Type: application/json' \
+    --data @./Data/user_test.json \
+    http://localhost:5000/accounts/create-user 
+
+curl --verbose \
+    -X POST \
+    --header 'Content-Type: application/json' \
+    --data @./Data/update_email.json \
+    http://localhost:2015/accounts/update-email
+
+curl --verbose \
+    -X POST \
+    --header 'Content-Type: application/json' \
+    --data @./Data/increment_karma.json \
+    http://localhost:2015/accounts/increment-karma
+
+curl --verbose \
+    -X POST \
+    --header 'Content-Type: application/json' \
+    --data @./Data/decrement_karma.json \
+    http://localhost:2015/accounts/decrement-karma
+
+curl --verbose \
+    -X PUT \
+    --header 'Content-Type: application/json' \
+    --data @./Data/user_test_2.json \
+    http://localhost:5000/accounts/create-user 
+
+curl --verbose \
+    -X POST \
+    --header 'Content-Type: application/json' \
+    --data @./Data/msg.json \
+    http://localhost:5000/msgs/send_msg
+
+curl --verbose \
+    -X POST \
+    --header 'Content-Type: application/json' \
+    --data @./Data/msg_id.json \
+    http://localhost:5000/msgs/fav_msg
+
+curl --verbose \
+    -X DELETE \
+    --header 'Content-Type: application/json' \
+    --data @./Data/msg_id.json \
+    http://localhost:5000/msgs/delete_msg
+
+curl --verbose \
+     -X POST \
+     --header 'Content-Type: application/json' \
+     --data @./Data/message.json \
+     http://localhost:5000/msgs/send_msg
+
+
+curl --verbose \
+     -X DELETE \
+     --header 'Content-Type: application/json' \
+     --data @./Data/delete.json \
+     http://localhost:5000/msgs/delete_msg
+
+curl --verbose \
+     -X POST \
+     --header 'Content-Type: application/json' \
+     --data @./Data/fav.json \
+     http://localhost:5000/msgs/fav_msg
+
+curl --verbose \
+     -X POST \
+     --header 'Content-Type: application/json' \
+     --data @./Data/unfav.json \
+     http://localhost:5000/msgs/unfav_msg
+
+curl --verbose \
+     -X POST \
+     --header 'Content-Type: application/json' \
+     --data @./Data/upvote.json \
+     http://localhost:5000/votes/up_vote
+
+
+curl --verbose \
+     -X POST \
+     --header 'Content-Type: application/json' \
+     --data @./Data/downvote.json \
+     http://localhost:5000/votes/down_vote
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/retrieve.json \
+     http://localhost:5000/votes/retrieve_votes
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/topvotes.json \
+     http://localhost:5000/votes/list_top_posts_byvotes
+
+curl --verbose \
+     -X GET \
+     --header 'Content-Type: application/json' \
+     --data @./Data/getList.json \
+     http://localhost:5000/votes/get_list
+
+curl --verbose \
+    -X DELETE \
+    --header 'Content-Type: application/json' \
+    --data @./Data/deactivate_account.json \
+    http://localhost:2015/accounts/deactivate-account
+
+
+
