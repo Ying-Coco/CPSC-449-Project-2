819a4caf6e1196bcf9bd4b06e59cd49ecc9efd30
bobberino98 <34085995+bobberino98@users.noreply.github.com>
2020-05-04 20:06:29 -0700

    Removed redundant files and created deployment directory

--- Deployment/TestSuite/load_test.sh
+++ Deployment/TestSuite/load_test.sh
@@ -1,2 +1,3 @@
 export PATH="$HOME/.local/bin:$PATH"
+
 locust -f load_test.py --host=http://localhost:2015
--- TestSuite/load_test.sh
+++
@@ -1,3 +0,0 @@
-export PATH="$HOME/.local/bin:$PATH"
-
-locust -f load_test.py --host=http://localhost:2015
